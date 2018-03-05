f8a82d456e97a9739b0c8fe4b781e6b452b261f2
olger <Olger1997@gmail.com>
2018-03-05 22:49:02 +0200

    Mäng toimib nüüd!

--- src/Mäng.java
+++ src/Mäng.java
@@ -1,6 +1,7 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Mäng {
+public class Mäng{
     public static void main(String[] arg){
         int ketasteArv;
         try {ketasteArv=Integer.parseInt(arg[0]);}
@@ -14,11 +15,19 @@ public class Mäng {
 
         Scanner skänner=new Scanner(System.in);
         String[] käsk;
-        while (true){
+        while (pulgad[0].kettad.size()!=0 || pulgad[1].kettad.size()!=0 || !Mäng.isSorted(pulgad[2].kettad)){
+            System.out.print(Mäng.isSorted(pulgad[2].kettad));
             System.out.println("pulk1:"+pulgad[0].kettad+"\npulk2:"+pulgad[1].kettad+"\npulk3:"+pulgad[2].kettad);
             käsk=skänner.next().split(",");
-            System.out.println("a"+käsk);
-            pulgad[Integer.parseInt(käsk[0])].tõstaKetasÜmber(pulgad[Integer.parseInt(käsk[1])]);
+            pulgad[Integer.parseInt(käsk[0])-1].tõstaKetasÜmber(pulgad[Integer.parseInt(käsk[1])-1]);
         }
+        System.out.print("Võitsid mängu!");
+    }
+    public static boolean isSorted(ArrayList<Ketas> data){//saadud aadressilt https://codereview.stackexchange.com/questions/58533/checking-if-an-array-is-sorted
+        for(int i = 1; i < data.size(); i++){
+            if(data.get(i-1).suurus > data.get(i).suurus){
+                return false;}
+        }
+        return true;
     }
 }
