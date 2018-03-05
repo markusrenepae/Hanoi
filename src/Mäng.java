92f3b86bd4275d056c20187f6377e066f7683ee0
olger <Olger1997@gmail.com>
2018-03-05 21:53:00 +0200

    Lisasin mägu õpetuse. Lisasin käsurea liidese, mängu mängimiseks.

--- src/Mäng.java
+++ src/Mäng.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Mäng {
     public static void main(String[] arg){
         int ketasteArv;
@@ -7,12 +9,16 @@ public class Mäng {
             System.out.println("Mängu ainsaks argumendiks peab olema ketaste arv, mis tuleb õigesse järjekorda panna.");
             return;
         }
+        System.out.println("Hanoi tornid (Brahma tornid) on matemaatiline mäng või puzzle, mis koosneb kolmest vardast ja erineva suurusega ketastest. Mängu alguses on kõik kettad esimese varda otsas kasvavas järjekorras suurim kõige all, tekitades koonuse. Mängu eesmärk on kõik kettad liigudata esimeselt vardalt kolmandale. Seejuures tuleb järgida kolme reeglit: korraga tohib liigutada ainult ühte ketast, iga käik tähendab pealmise ketta võtmist ja selle asetamist teise varda otsa, kusjuures tuleb järgida, et ei asetatakse suuremat ketast väiksema peale(rikkudes sellega koonuse kuju).");
         Pulk pulk1=new Pulk();
         pulk1.täidaKetastega(ketasteArv);
         Pulk pulk2=new Pulk();
         Pulk pulk3=new Pulk();
-        System.out.println(pulk1.kettad);
-        pulk1.tõstaKetasÜmber(pulk2);
-        System.out.println(pulk1.kettad+";"+pulk2.kettad);
+
+        Scanner skänner=new Scanner(System.in);
+        String[] käsk;
+        while (true){
+            käsk=skänner.next().split(",");
+            System.out.println("a"+käsk);}
     }
 }
