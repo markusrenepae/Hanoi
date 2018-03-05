53c7bc4e820e920866446e68372f23db04352c41
olger <Olger1997@gmail.com>
2018-03-05 21:33:32 +0200

    Eemaldasin ebavajaliku Main klassi.

--- src/Mäng.java
+++ src/Mäng.java
@@ -1,11 +1,18 @@
 public class Mäng {
     public static void main(String[] arg){
-    Pulk pulk1=new Pulk();
-    pulk1.täidaKetastega(8);
-    Pulk pulk2=new Pulk();
-    Pulk pulk3=new Pulk();
-    System.out.println(pulk1.kettad);
-    pulk1.tõstaKetasÜmber(pulk2);
-    System.out.println(pulk1.kettad+";"+pulk2.kettad);
+        int ketasteArv;
+        try {
+            ketasteArv=Integer.parseInt(arg[0]);}
+        catch (Exception e){
+            System.out.println("Mängu ainsaks argumendiks peab olema ketaste arv, mis tuleb õigesse järjekorda panna.");
+            return;
+        }
+        Pulk pulk1=new Pulk();
+        pulk1.täidaKetastega(ketasteArv);
+        Pulk pulk2=new Pulk();
+        Pulk pulk3=new Pulk();
+        System.out.println(pulk1.kettad);
+        pulk1.tõstaKetasÜmber(pulk2);
+        System.out.println(pulk1.kettad+";"+pulk2.kettad);
     }
 }
